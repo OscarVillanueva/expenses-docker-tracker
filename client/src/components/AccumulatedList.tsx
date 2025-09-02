98b454e47eeda53cd0c18ebcb892e0fd66a5f866
Javier Villanueva <javier.villanueva@tryrook.io>
2025-09-02 14:21:15 -0600

    feat(accumulatedlist.tsx): include the list of acc into zustand

--- client/src/components/AccumulatedList.tsx
+++ client/src/components/AccumulatedList.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { type FC, useEffect } from 'react';
 import { type AccumulatedResponse } from '../types/AccumulatedResponse'
 import { accumulatedState } from '../state/accumulatedState'
 import Accumulated from './Accumulated';
@@ -8,9 +8,17 @@ interface AccumulatedListProps {
 }
 
 const AccumulatedList: FC<AccumulatedListProps> = ({ items }) => {
+  const list = accumulatedState(state => state.list)
   const updateAcc = accumulatedState(state => state.updateAccumulated)
+  const updateList = accumulatedState(state => state.setList)
+
+  useEffect(() => {
+    updateList(items.data)
+  }, [])
+  
 
   const handleUpdateAcc = (id: string, value: string) => {
+    const l = list.find(e => e.uuid === id)
     updateAcc(Number(value))
   }
 
--- client/src/state/accumulatedState.ts
+++ client/src/state/accumulatedState.ts
@@ -1,11 +1,16 @@
 import { create } from 'zustand'
+import { type Accumulated } from '../types/AccumulatedResponse'
 
 type AccumulatedState = {
+  list: Accumulated[]
   accumulated: number;
   updateAccumulated: (acc: number) => void
+  setList: (list: Accumulated[]) => void
 }
 
 export const accumulatedState = create<AccumulatedState>((set) => ({
+  list: [],
   accumulated: 0,
-  updateAccumulated: (acc: number) => set({ accumulated: acc})
+  updateAccumulated: (acc: number) => set({ accumulated: acc}),
+  setList: (list: Accumulated[]) => set({ list })
 }))
